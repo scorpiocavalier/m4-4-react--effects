a7411df22999fc23f38cf22d3e661a5e2119a155
Arturo Chen <art.can.fly@gmail.com>
2020-07-27 02:10:56 -0400

    created data.js and Items component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,21 +1,16 @@
 import React, { useState }  from "react"
 import { Link }             from "react-router-dom"
 import styled               from "styled-components"
-import Item                 from './Item'
+import Items                from './Items'
 import cookieSrc            from "../cookie.svg"
 import useInterval          from '../hooks/useInterval'
 import useKeydown           from '../hooks/useKeydown'
 import useDocumentTitle     from '../hooks/useDocumentTitle'
+import { items }            from '../data'
 
 export default () => {
 
-  const items = [
-    { id: "cursor", name: "Cursor", cost: 10, value: 1 },
-    { id: "grandma", name: "Grandma", cost: 100, value: 10 },
-    { id: "farm", name: "Farm", cost: 1000, value: 80 },
-  ]
-
-  const [numCookies, setNumCookies] = useState(100)
+  const [numCookies, setNumCookies] = useState(1000)
 
   const [purchasedItems, setPurchasedItems] = useState({
     cursor: 0, grandma: 0, farm: 0,
@@ -75,17 +70,7 @@ export default () => {
 
       <ItemArea>
         <SectionTitle>Items:</SectionTitle>
-        { items.map((item, index) => {
-          return (
-            <Item
-              key={item.id}
-              item={item}
-              purchasedItems={purchasedItems}
-              handleAttemptedPurchase={() => handleAttemptedPurchase(item)}
-              isFirst={index === 0}
-            />
-          )
-        })}
+        <Items purchasedItems={purchasedItems} handleAttemptedPurchase={handleAttemptedPurchase} />
       </ItemArea>
       <HomeLink to="/">Return home</HomeLink>
     </Wrapper>
